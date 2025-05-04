5b62390480b0b9849b031b7b83e15d57ea3e5735
Adi <adityasagarpython@gmail.com>
2025-05-04 00:12:32 -0300

    feat: Cogwheel floats now

--- Assets/Code/Scripts/Items/Pickable/CogPickable.cs
+++ Assets/Code/Scripts/Items/Pickable/CogPickable.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class CogPickable : BasePickable
 {
-    [SerializeField] private AudioSource pickupSound;
+    [SerializeField] private AudioClip pickupSound;
+    [SerializeField] private float xRotationSpeed = 10.0f;
+    [SerializeField] private float yRotationSpeed = 10.0f;
+    [SerializeField] private float pickupSoundVolume = 0.4f;
     public override void Pickup()
     {
         base.Pickup();
-        pickupSound.Play();
+        AudioSource.PlayClipAtPoint(pickupSound ,transform.position, pickupSoundVolume);
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.CompareTag("Player")) Pickup();
-        Destroy(gameObject);
+        if (collision.CompareTag("Player"))
+        {
+            Pickup();
+            Destroy(gameObject);
+        }
+     
     }
 
     private void Update()
     {
-       
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        
+        // calculate rotations
+        float deltaX = xRotationSpeed * Time.deltaTime;
+        float deltaY = yRotationSpeed * Time.deltaTime;
+
+        Vector3 newRotation = new Vector3(
+            currentRotation.x + deltaX,
+            currentRotation.y + deltaY,
+            0f);
+
+        transform.rotation = Quaternion.Euler(newRotation);
     }
 
 }
